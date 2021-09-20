66043319e994f03f1b0739bb6232b0ebaf18a4a5
danica-du <ddu@hmc.edu>
2021-09-20 14:26:46 -0700

    Finished in-class exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = [
+        {
+            id: 512,
+            name: "Neil Rhodes",
+            email: "rhodes@hmc.edu",
+            phone: "(909) 555-1212"
+        },
+        {
+            id: 787,
+            name: "Barack Obama",
+            email: "ex-prez@whitehouse.gov",
+            phone: "(312) 555-1212"
+        }
+    ];
+
+const listItems = data.map(
+    e => <li key={e.id}> <div id="name">{e.name}</div> <div id="email">{e.email}</div> <div id="phone">{e.phone}</div> </li>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+        <h1>People</h1>
+        <ul>
+            {listItems}
+        </ul>
+    </div>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
   document.getElementById('root')
 );
 
